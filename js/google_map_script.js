c325b4d9cfb062434ba0991f5483d5ab03a8fd54
Fangzhou Sun <fzsun316@gmail.com>
2015-03-25 11:29:29 -0500

    Added the register button and two hotel links; Also fixed some bugs;

--- js/google_map_script.js
+++ js/google_map_script.js
@@ -3,6 +3,8 @@ function initialize() {
 	new google.maps.LatLng(36.156684, -86.778655),
 new google.maps.LatLng(36.151556, -86.794362), //Home2 Suites
 new google.maps.LatLng(36.152171, -86.794788), //Hilton Garden Inn
+new google.maps.LatLng(36.153216, -86.796459), //SpringHill Suites Nashville Vanderbilt/West End
+new google.maps.LatLng(36.153623, -86.796758), //Residence Inn Nashville Vanderbilt/West End
 new google.maps.LatLng(36.151860, -86.797688), //Courtyard Nashville 
 new google.maps.LatLng(36.148138, -86.806881)  //Homewood Suites by Hilton® Nashville Vanderbilt, TN
 ]
@@ -11,6 +13,18 @@ var mapCanvas = document.getElementById('map-canvas');
 var mapOptions = {
 	// center: latLngArray[0],
 	// zoom: 14,
+	mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DEFAULT,
+      mapTypeIds: [
+        google.maps.MapTypeId.ROADMAP,
+        google.maps.MapTypeId.TERRAIN
+      ]
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 var map = new google.maps.Map(mapCanvas, mapOptions);
@@ -21,6 +35,8 @@ bounds.extend(latLngArray[1]);
 bounds.extend(latLngArray[2]);
 bounds.extend(latLngArray[3]);
 bounds.extend(latLngArray[4]);
+bounds.extend(latLngArray[5]);
+bounds.extend(latLngArray[6]);
 map.fitBounds(bounds);
 
 for (i=1; i<latLngArray.length; i++) {
